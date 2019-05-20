6300264b6d24b217f58acad0e850c32084784bda
salaboy <salaboy@gmail.com>
2019-05-20 11:09:01 +0200

    refactoring a little bit

--- src/main/java/org/salaboy/sck8s/serviceb/ServiceBApplication.java
+++ src/main/java/org/salaboy/sck8s/serviceb/ServiceBApplication.java
@@ -1,5 +1,7 @@
 package org.salaboy.sck8s.serviceb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ServiceBApplication implements CommandLineRunner {
 
-    private Logger logger = LoggerFactory.getLogger(ServiceAApplication.class);
+    private Logger logger = LoggerFactory.getLogger(ServiceBApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceBApplication.class,
